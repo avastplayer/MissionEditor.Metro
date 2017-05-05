56ed6bd48b8eee1fbf24b8c4d4171d9e3a3f42e1
嘉诚 卢 <lujiacheng45@hotmail.com>
2017-05-06 00:25:49 +0800

    新建Metro项目

--- MissionEditor.Metro/MainWindow.xaml.cs
+++ MissionEditor.Metro/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,18 +13,91 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MahApps.Metro.Controls;
 
 namespace MissionEditor.Metro
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
+        public List<int> WriteRows { get; set; } = new List<int>();
+        public int SelectDataRow { get; set; }
+        public AssetManager AssetManager = new AssetManager();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MissionDataGrid.ItemsSource = GetExcelData(AssetManager.MissionDatatable);
+            //MissionListView.ItemsSource = assetManager.MissionDatatable.DefaultView;
+
+            DataProcess dataProcess = new DataProcess();
+            dataProcess.CreateXmlFile();
+
+            //assetManager.GetNpcInfo(14039, out string npcName, out int headId, out string mapName);
+            //Console.WriteLine(npcName);
+            //Console.WriteLine(mapName);
+
+            //int imagesetName = (headId - 9000) / 4;
+            //Head.Source = assetManager.GetImage(assetManager.HeadImageset + imagesetName, headId.ToString());
+        }
+
+        private static DataView GetExcelData(DataTable missionDatatable)
+        {
+            string[] strColumns = { "MissionID", "MissionName", "MissionTypeString" };
+            return missionDatatable.DefaultView.ToTable(missionDatatable.TableName, true, strColumns).DefaultView;
+        }
+
+        private void MissionDataGrid_click(object sender, MouseButtonEventArgs e)
+        {
+            SelectDataRow = GetSelectedRow();
+
+            Binding binding = new Binding()
+            {
+                Source = AssetManager.MissionDatatable.Rows[SelectDataRow],
+                Path = new PropertyPath("[ActiveInfoNpcID]"),
+                Mode = BindingMode.TwoWay
+            };
+            ActiveInfoNpcIDTextBox.SetBinding(TextBox.TextProperty, binding);
+            //Binding binding = new Binding()
+            //{
+            //    Source = AssetManager.MissionDatatable,
+            //    Path = new PropertyPath("[0][ActiveInfoNpcID]")
+            //};
+            //ActiveInfoNpcIDTextBox.SetBinding(TextBox.TextProperty, binding);
+        }
+
+        private int GetSelectedRow()
+        {
+            if (MissionDataGrid != null && MissionDataGrid.SelectedCells.Count != 0)
+            {
+                return MissionDataGrid.SelectedIndex;
+            }
+
+            return -1;
+        }
+
+        //private void BindTextBoxValue(TextBox textBox, string value)
+        //{
+        //    AssetManager assetManager = new AssetManager();
+        //    int selectDataRow = GetSelectedRow();
+        //    ;
+        //    Binding binding = new Binding()
+        //    {
+        //        Source = assetManager.MissionDatatable,
+        //        Path = new PropertyPath("[selectDataRow][ActiveInfoNpcID]")
+        //    };
+        //    textBox.SetBinding(TextBox.TextProperty, binding);
+        //}
+        private void SaveButton_click(object sender, MouseButtonEventArgs e)
+        {
+            AssetManager.SaveExcel(WriteRows);
+        }
+
+        private void TextBox_TextInput(object sender, TextChangedEventArgs textChangedEventArgs)
+        {
+            WriteRows.Add(SelectDataRow);
         }
     }
 }
